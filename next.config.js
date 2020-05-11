517cdf7c06d42f1048dc7cd1eb9a367d81d461f6
Scott Hutcheson <smartinhutcheson@gmail.com>
2020-05-11 21:19:54 +0100

    aliases

--- next.config.js
+++ next.config.js
@@ -8,12 +8,6 @@ module.exports = (_phase, { defaultConfig }) => {
       GA_TRACKING_ID: process.env.GA_TRACKING_ID,
     },
     pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-    webpack: (config) => {
-      // import aliases to avoid long import paths.
-      config.resolve.alias['components'] = `${__dirname}/components`
-      config.resolve.alias['lib'] = `${__dirname}/lib`
-      return config
-    },
   }
 
   if (process.env.ANALYZE === 'true') {
