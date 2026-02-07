06ea1953410bd2db96ca49a5056d78ea0f39750e
Scott Hutcheson <scott@smhutch.co.uk>
2026-02-07 07:10:34 +0000

    Fix React type mismatch with markdoc (#83)

--- components/markdoc.tsx
+++ components/markdoc.tsx
@@ -3,8 +3,13 @@ import type { RenderableTreeNodes } from '@markdoc/markdoc'
 import React from 'react'
 import { MARKDOC_COMPONENTS } from './markdown'
 
+type MarkdocRenderer = typeof Markdoc.renderers.react
+type MarkdocRendererParams = Parameters<MarkdocRenderer>
+type MarkdocRendererReactParam = MarkdocRendererParams[1]
+const markdocReact = React as MarkdocRendererReactParam
+
 export const renderMarkdoc = (content: RenderableTreeNodes) => {
-  return Markdoc.renderers.react(content, React, {
+  return Markdoc.renderers.react(content, markdocReact, {
     components: MARKDOC_COMPONENTS,
     resolveTagName: (name) => {
       const component =
