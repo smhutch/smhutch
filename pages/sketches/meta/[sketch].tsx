a1a5760e2650573cddaf59c984cfb351c399874d
Scott Hutcheson <scott@smhutch.co.uk>
2026-02-13 11:43:52 +0000

    wip

--- pages/sketches/meta/[sketch].tsx
+++ pages/sketches/meta/[sketch].tsx
@@ -1,7 +1,6 @@
-import { createRandom } from 'canvas-sketch-util/random'
-import type { NextPage } from 'next'
-
 import { Sketch } from 'layouts/Sketch'
+import { createRandom } from 'lib/random'
+import type { NextPage } from 'next'
 import type { SketchInitialProps } from 'types/sketches'
 
 const random = createRandom()
